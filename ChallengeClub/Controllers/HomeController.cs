e53b3ddbe7b39aea18bfb26aafab73e374d416db
MaoJing520 <44821032+MaoJing520@users.noreply.github.com>
2019-01-25 14:25:06 -0500

    Initial commit

---
+++ ChallengeClub/Controllers/HomeController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ChallengeClub.Models;
+using System.Text;
+
+namespace ChallengeClub.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult MemberLogin()
+        {
+            return View();
+        }
+
+        public IActionResult Icon()
+        {
+
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MemberActivity()
+        {
+            List<DailyActivity> dact = new List<DailyActivity>();
+
+            dact.Add(new DailyActivity()
+            {
+                activityId = 1,
+                activityName = "Bowling",
+                startTime = "10 am",
+                isCheck = false
+            });
+
+            dact.Add(new DailyActivity()
+            {
+                activityId = 2,
+                activityName = "Pet Therapy",
+                startTime = "1 pm",
+                isCheck = false
+            });
+
+            dact.Add(new DailyActivity()
+            {
+                activityId = 3,
+                activityName = "Volunteer",
+                startTime = "3 pm",
+                isCheck = false
+            });
+
+            dact.Add(new DailyActivity()
+            {
+                activityId = 4,
+                activityName = "Whitey's Dinner",
+                startTime = "6 pm",
+                isCheck = false
+            });
+
+            dact.Add(new DailyActivity()
+            {
+                activityId = 5,
+                activityName = "Dancing",
+                startTime = "8 pm",
+                isCheck = false
+            });
+
+            actList alist = new actList();
+
+            alist.dailyActs = dact;
+
+            return View(alist);
+
+            /*DailyActivity bowling = new DailyActivity
+            {
+                activityName = "Bowling",
+                startTime = "10 am",
+            };
+
+            DailyActivity pet = new DailyActivity
+            {
+                activityName = "Pet Therapy",
+                startTime = "1 pm",
+            };
+
+            DailyActivity volunteer = new DailyActivity
+            {
+                activityName = "Volunteer",
+                startTime = "1 pm",
+            };
+
+            DailyActivity wDinner = new DailyActivity
+            {
+                activityName = "Whitey's Dinner",
+                startTime = "6 pm",
+            };
+
+            DailyActivity dancing = new DailyActivity
+            {
+                activityName = "Dancing",
+                startTime = "8 pm",
+            };*/
+
+        }
+        [HttpPost]
+        public IActionResult MemberActivity(actList ac)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var thing in ac.dailyActs)
+            {
+                if (thing.isCheck) {
+                    sb.Append(thing.activityName + " at " + thing.startTime + ",  ");
+                }
+            }
+            ViewBag.selectAct = "Your Activities are " + sb.ToString();
+            return View(ac);
+        }
+
+        public IActionResult Confirm()
+        {
+
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChallengeClub/Models/DailyActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeClub.Models
+{
+    public class DailyActivity
+    {
+        public int activityId { get; set; }
+        public string activityName { get; set; }
+        public string startTime { get; set; }
+        public bool isCheck { get; set; }
+    }
+
+    public class actList
+    {
+        public List<DailyActivity> dailyActs { get; set; }
+
+    }
+}

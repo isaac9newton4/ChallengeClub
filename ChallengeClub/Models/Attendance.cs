a4f518d9c6f562649caf01f6709185622df1246c
Isaac Newton <sirnewton@Isaacs-MacBook-Pro.local>
2019-04-16 16:41:51 -0400

    commiting

---
+++ ChallengeClub/Models/Attendance.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ChallengeClub.Models
+{
+    public class MemberLogin
+    {
+        public MemberLogin()
+        {
+        }
+    }
+}
---
+++ ChallengeClub/Repositories/MemberLoginRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ChallengeClub.Repositories
+{
+    public class MemberLoginRepository
+    {
+        public MemberLoginRepository()
+        {
+        }
+    }
+}
